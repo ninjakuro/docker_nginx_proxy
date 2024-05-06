5c1f37f505107fccf3b4900d77309cf961ac83e4
Viktor Astakhov <animelayer@gmail.com>
2024-05-06 15:15:11 +0300

    initial commit

--- proxy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker network create mpc-network
-docker compose -f ../web/docker-compose.yml up --build -d
-docker compose -f ../api/docker-compose.yml up --build -d
-docker compose -f ./docker-compose.yml up -d
--- proxy/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker compose -f ./docker-compose.yml down
-docker compose -f ../web/docker-compose.yml down
-docker compose -f ../api/docker-compose.yml down
-docker network rm mpc-network
